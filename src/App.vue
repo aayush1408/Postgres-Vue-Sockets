4f6e8afeba9c1f67f233151e433904134deaea20
aayush1408 <aayushjaiswal0114@gmail.com>
2018-06-15 20:11:24 +0530

    styling maintained

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,40 @@
 <template>
-  <div id="app" class="row">
-    <div class="col-lg-6">
-      <ul class="list-group" v-for="block in blocks" :key="block.block_number">       
-        <li class="list-group-item">
-        <router-link v-bind:to="'/block/'+block.block_number">
-        Block-no {{block.block_number}}
-        Block-hash {{block.block_hash}}
-        Mined by {{block.miner}}      
-        TimeStamp {{block.timestamp}}
+  <div id="app">
+    <!-- navabar -->
+    <nav class="navbar navbar-light bg-light">
+      <span class="navbar-brand mb-0 h1">Matic Network</span>
+    </nav>
+    <div  class="container">
+    <div class="row">
+      <div class="col-lg-6" id="block" style=" display:block; width:500px; height:500px;overflow-y:scroll;overflow-x:hidden;">
+        <div class="list-group" v-for="block in blocks" :key="block.block_number">       
+          <div class="list-group-item" style="display:block;heigth:100px;">
+          <router-link v-bind:to="'/block/'+block.block_number">
+          <ul>
+          <li>Block-no {{block.block_number}}</li>
+          <li>Block-hash {{block.block_hash}}</li>
+          <li>Mined by {{block.miner}}</li>      
+          <li>TimeStamp {{block.timestamp}}</li>
+          </ul>
+          </router-link>
+          </div>
+        </div>   
+      </div>
+      <div class="col-lg-6" id="transaction" style=" display:block; width:300px; height:500px;overflow-y:scroll;overflow-x:hidden;" >
+        <div class="list-group" v-for="transaction in transactions" :key="transaction.block_number">
+        <div class="list-group-item" style="display:block;heigth:100px;">            
+          <router-link v-bind:to="'/transaction/'+transaction.block_number">
+          <ul>
+          <li>TransactionHash {{transaction.transaction_hash}}</li>
+          <li>Sender  {{transaction.sender}}</li>
+          <li>Reciever {{transaction.receiver}}</li>
+          </ul>
         </router-link>
-        </li>
-      </ul>   
+        </div> 
+        </div>  
+      </div>      
     </div>
-    <div class="col-lg-6">
-      <ul class="list-group" v-for="transaction in transactions" :key="transaction.block_number">
-      <li class="list-group-item">            
-        <router-link v-bind:to="'/transaction/'+transaction.block_number">
-        TransactionHash {{transaction.transaction_hash}}
-        Sender  {{transaction.sender}}
-        Reciever {{transaction.receiver}}
-        <!-- Data  {{transaction.data}} -->
-       </router-link>
-       </li> 
-      </ul>  
-    </div>      
+   </div>
   </div>
 </template>
 
@@ -53,6 +64,25 @@ export default {
 
 }
 </script>
+<style>
+li{
+  overflow: hidden;
+}
+#transaction::-webkit-scrollbar {
+    width: 3px;
+    background-color: #F5F5F5;
+} 
+#block::-webkit-scrollbar {
+    width: 3px;
+    background-color: #F5F5F5;
+} 
+#block::-webkit-scrollbar-thumb {
+    background-color: #000000;
+}
+#transaction::-webkit-scrollbar-thumb {
+    background-color: #000000;
+}
+</style>
 
 
 
