bd87319e6b2a28dea68941ddd473a02dd6a23004
aayush1408 <aayushjaiswal0114@gmail.com>
2018-06-18 13:35:23 +0530

    styles added

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,38 @@
 <template>
-  <div id="app">
+  <div id="app" class="">
     <!-- navabar -->
     <nav class="navbar navbar-light bg-light">
       <span class="navbar-brand mb-0 h1">Matic Network</span>
     </nav>
+    <!-- body part of the page -->
     <div  class="container">
     <div class="row">
-      <div class="col-lg-6" id="block" style=" display:block; width:500px; height:500px;overflow-y:scroll;overflow-x:hidden;">
-        <div class="list-group" v-for="block in blocks" :key="block.block_number">       
-          <div class="list-group-item" style="display:block;heigth:100px;">
-          <router-link v-bind:to="'/block/'+block.block_number">
-          <ul>
-          <li>Block-no {{block.block_number}}</li>
-          <li>Block-hash {{block.block_hash}}</li>
-          <li>Mined by {{block.miner}}</li>      
-          <li>TimeStamp {{block.timestamp}}</li>
-          </ul>
-          </router-link>
+      <!-- contains the block and transactions -->
+      <div class="col-lg-6 col-md-12 col-sm-12" id="block">
+        <div class="list-group">       
+          <div class="list-group-item" v-for="block in blocks" :key="block.block_number">
+            <router-link v-bind:to="'/block/'+block.block_number">
+              <ul id="block-list">
+              <li id="block-number"><span id="inside-box">Block {{block.block_number}}</span></li>
+              <li id="block-hash">Block-hash {{block.block_hash}}</li>
+              <li>Miner{{block.miner}}</li>      
+              <li>TimeStamp {{block.timestamp}}</li>
+              </ul>          
+            </router-link>
           </div>
         </div>   
       </div>
-      <div class="col-lg-6" id="transaction" style=" display:block; width:300px; height:500px;overflow-y:scroll;overflow-x:hidden;" >
-        <div class="list-group" v-for="transaction in transactions" :key="transaction.block_number">
-        <div class="list-group-item" style="display:block;heigth:100px;">            
-          <router-link v-bind:to="'/transaction/'+transaction.block_number">
-          <ul>
-          <li>TransactionHash {{transaction.transaction_hash}}</li>
-          <li>Sender  {{transaction.sender}}</li>
-          <li>Reciever {{transaction.receiver}}</li>
-          </ul>
-        </router-link>
-        </div> 
+      <div class="col-lg-6 col-md-12 col-sm-12" id="transaction">
+        <div class="list-group">
+          <div class="list-group-item" v-for="transaction in transactions" :key="transaction.block_number">            
+            <router-link v-bind:to="'/transaction/'+transaction.block_number">
+              <ul id="list-transactions">
+              <li id="transaction-hash"><span class="transaction-details" id="transaction-hash">TransactionHash</span> {{transaction.transaction_hash}}</li>
+              <li><span class="transaction-details">Sender {{transaction.sender}}</span></li>
+              <li><span class="transaction-details">Reciever {{transaction.receiver}}</span></li>
+              </ul>
+            </router-link>
+          </div> 
         </div>  
       </div>      
     </div>
@@ -65,9 +67,54 @@ export default {
 }
 </script>
 <style>
-li{
+#block,#transaction{
+ display:block; 
+ width:200px;
+ height:500px;
+ overflow-y:scroll;
+ overflow-x:hidden;
+ border-spacing: 200px;
+}
+#block-number{
+  display:block;
+  height:50px;
+  width:120px;
+  float:left;
+  background-color:darkgray;
+  color:white;
+}
+#list-transactions{
+  list-style: none;
+  color:grey;
+  font-family: 'Times New Roman', Times, serif;
+}
+#block-list{
+  color:grey;
+  font-family: 'Times New Roman', Times, serif;
+  list-style: none;
+  
+}
+#block-hash{
   overflow: hidden;
 }
+#transaction-hash{
+  overflow: hidden;
+}
+.row{
+  margin-top: 10px;
+}
+.list-group{
+  margin-left: -30px;
+}
+.list-group-item{
+  display: inline;
+}
+#transaction-hash{
+  /* display: block; */
+  float: left;
+}
+
+
 #transaction::-webkit-scrollbar {
     width: 3px;
     background-color: #F5F5F5;
@@ -77,10 +124,10 @@ li{
     background-color: #F5F5F5;
 } 
 #block::-webkit-scrollbar-thumb {
-    background-color: #000000;
+    background-color:darkgray;
 }
 #transaction::-webkit-scrollbar-thumb {
-    background-color: #000000;
+    background-color:darkgray;
 }
 </style>
 
