6486accbe252f2ddfbd008588e9d617a27547a37
aayush1408 <aayushjaiswal0114@gmail.com>
2018-06-22 15:08:32 +0530

    route added for block transactions

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,10 @@
-import Vue from 'vue'
-import Router from 'vue-router'
+import Vue from 'vue';
+import Router from 'vue-router';
 import App from '@/App';
+import BlockTransactions from '../pages/BlockTransactions';
 import Block from '@/pages/Block';
 import Transaction from '@/pages/Transaction';
+
 Vue.use(Router)
 export default new Router({
     routes: [
@@ -20,6 +22,11 @@ export default new Router({
         path: '/',
         name: 'App',
         component: App
+      },
+      {
+        path: '/block/transactions/:id',
+        name: 'BlockTransactions',
+        component: BlockTransactions
       }
     ]
   });
